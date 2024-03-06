fccccaf8acecdbdd194f9e3effa55641a6ea24c7
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-03-06 23:29:40 +0400

    Add files via upload

---
+++ Casting_practise/Program.cs
@@ -0,0 +1,107 @@
+﻿
+//int a = 5;
+
+//object b = a;
+
+
+//int a = 6;
+
+//object b = a;
+
+//object b = 8;
+
+//object b = true;
+
+//object b = 100;
+
+//int c = (int)b;
+
+//object b = "salam";
+
+//int c = int.Parse(b);
+
+//string c = b.ToString();
+
+//byte num = 50;
+
+//int num2 = num;
+
+//int a = 10;
+
+//byte b = (byte) a;
+
+//Console.WriteLine(b);
+
+////Console.WriteLine("");
+
+//using Casting_practise;
+
+//Fish fish = new Fish();
+//Animal animal1 = fish;
+
+////Animal animal2 = new Animal();
+
+////Fish fish1 = (Fish)animal2;
+
+//Animal animal3 = new Dog();
+//Animal animal4 = new Fish();
+//Animal animal = new();
+//Fish fish1 = (Fish)animal;
+//Dog dog = new Dog();
+
+
+
+////object[] datas = { 1, 2, true, "salam" };
+
+//object[] animals = (animal1, animal3, animal4, dog, "sagfd");
+//foreach (var item is Animal)
+//{
+//    Animal data = item as Animal;
+//    //Console.WriteLine(data);
+
+//if(data is not null)
+//{
+//    data.Name = "akula";
+//    Console.WriteLine(data.Name);
+//}
+
+//Console.WriteLine(item is Animal);
+//var result = (Animal)item;
+//((Animal)item).Name = "toplan";
+//Console.WriteLine(((Animal)item).Name);
+
+
+//if (item is Animal)
+//{
+//    Console.WriteLine("yes");
+//}
+
+//if (item is Dog)
+//{
+//    Console.WriteLine();
+//}
+
+//if (item is Animal a)
+//{
+//    a.Name = "toplan";
+//    Console.WriteLine(a.Name);
+//}
+//}
+
+//DataList dataList = new DataList();
+//dataList[0] = "salam";
+//dataList[1] = "sa]ol";
+
+//Console.WriteLine(dataList[0]);
+
+
+using Casting_practise;
+
+Employee employee1 = new();
+employee1.Id = 1;
+employee1.Age = 24;
+Employee employee2 = new();
+employee2.Id = 2;
+employee2.Age = 30;
+Console.WriteLine(employee1 > employee2);
+
